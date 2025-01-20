eec2772c34dcd7a103a35675c59cff6664bb5e25
canizalesjaime <canizales.jaime16@gmail.com>
2025-01-19 19:28:39 -0500

    heroku

--- migrate.sh
+++
@@ -1,2 +0,0 @@
-export PGPASSWORD="hello1234"
-psql -U postgres -d postgres -f /migrations/init.sql
--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@
 service postgresql start
 npm install
 node migrate.js
+
+#or
+# export PGPASSWORD="hello1234"
+# psql -U postgres -d postgres -f /migrations/init.sql
+ 
 # Prevent the container from exiting
 tail -f /dev/null
